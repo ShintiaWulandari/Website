ca4edcd90a9d311e8eb0cd363e09feb83c3f752e
oriorasajambu <oriorasajambu@gmail.com>
2021-12-05 10:46:35 +0700

    fix sql query

--- admin/hapus.php
+++ admin/hapus.php
@@ -1,67 +1,61 @@
-<?php 
+<?php
 
-	include '../koneksi.php';
+include '../koneksi.php';
 
-	if (isset($_GET['idjurusan'])){
+if (isset($_GET['idjurusan'])) {
 
-		$jurusan = mysqli_query($conn, "SELECT gambar FROM jurusan id= '".$_GET['idjurusan']."' ");
+	$jurusan = mysqli_query($conn, "SELECT gambar FROM jurusan WHERE id= '" . $_GET['idjurusan'] . "' ");
 
-		if(mysqli_num_rows($jurusan) > 0){
+	if (mysqli_num_rows($jurusan) > 0) {
 
-			$p = mysqli_fetch_object($jurusan);
+		$p = mysqli_fetch_object($jurusan);
 
-			if(file_exists("../uploads/jurusan/".$p->gambar)){
+		if (file_exists("../uploads/jurusan/" . $p->gambar)) {
 
-				unlink("../uploads/jurusan/".$p->gambar);
-			}
+			unlink("../uploads/jurusan/" . $p->gambar);
 		}
+	}
 
-		$delet = mysqli_query($conn, "DELETE FROM jurusan WHERE id = '".$_GET['idjurusan']."' ");
-		echo "<script>window.location = 'jurusan.php'</script>";
+	$delet = mysqli_query($conn, "DELETE FROM jurusan WHERE id = '" . $_GET['idjurusan'] . "' ");
+	echo "<script>window.location = 'jurusan.php'</script>";
+}
 
+include '../koneksi.php';
+
+if (isset($_GET['idgaleri'])) {
+
+	$galeri = mysqli_query($conn, "SELECT foto FROM galeri WHERE id= '" . $_GET['idgaleri'] . "' ");
+
+	if (mysqli_num_rows($galeri) > 0) {
+
+		$p = mysqli_fetch_object($galeri);
+
+		if (file_exists("../uploads/galeri/" . $p->foto)) {
+
+			unlink("../uploads/galeri/" . $p->foto);
 		}
+	}
 
-		include '../koneksi.php';
+	$delet = mysqli_query($conn, "DELETE FROM galeri WHERE id = '" . $_GET['idgaleri'] . "' ");
+	echo "<script>window.location = 'galeri.php'</script>";
+}
 
-		if (isset($_GET['idgaleri'])){
+include '../koneksi.php';
 
-			$galeri = mysqli_query($conn, "SELECT foto FROM galeri id= '".$_GET['idgaleri']."' ");
+if (isset($_GET['idinformasi'])) {
 
-			if(mysqli_num_rows($galeri) > 0){
+	$informasi = mysqli_query($conn, "SELECT gambar FROM informasi WHERE id= '" . $_GET['idinformasi'] . "' ");
 
-				$p = mysqli_fetch_object($galeri);
+	if (mysqli_num_rows($informasi) > 0) {
 
-				if(file_exists("../uploads/galeri/".$p->foto)){
+		$p = mysqli_fetch_object($informasi);
 
-					unlink("../uploads/galeri/".$p->foto);
-				}
-			}
+		if (file_exists("../uploads/informasi/" . $p->foto)) {
 
-			$delet = mysqli_query($conn, "DELETE FROM galeri WHERE id = '".$_GET['idgaleri']."' ");
-			echo "<script>window.location = 'galeri.php'</script>";
+			unlink("../uploads/informasi/" . $p->foto);
+		}
+	}
 
-			}
-
-			include '../koneksi.php';
-
-			if (isset($_GET['idinformasi'])){
-
-				$informasi= mysqli_query($conn, "SELECT foto FROM informasi id= '".$_GET['idinformasi']."' ");
-
-				if(mysqli_num_rows($informasi) > 0){
-
-					$p = mysqli_fetch_object($informasi);
-
-					if(file_exists("../uploads/informasi/".$p->foto)){
-
-						unlink("../uploads/informasi/".$p->foto);
-					}
-				}
-
-				$delet = mysqli_query($conn, "DELETE FROM informasi WHERE id = '".$_GET['idinformasi']."' ");
-				echo "<script>window.location = 'informasi.php'</script>";
-
-			}
-
-
- ?>
+	$delet = mysqli_query($conn, "DELETE FROM informasi WHERE id = '" . $_GET['idinformasi'] . "' ");
+	echo "<script>window.location = 'informasi.php'</script>";
+}
